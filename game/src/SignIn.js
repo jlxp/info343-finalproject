7efc8226a01329e18c537a87dec44d8d6e76e823
Joy Phillips <jlxp@uw.edu>
2018-03-04 15:57:35 -0800

    finished signin page

--- game/src/SignIn.js
+++ game/src/SignIn.js
@@ -10,6 +10,7 @@ export default class SignInView extends React.Component {
         this.state = {
             currentUser: undefined,
             userName: undefined,
+            userSnap: undefined,
             userRef: undefined
         };
         firebase.auth().signInAnonymously();
@@ -21,9 +22,9 @@ export default class SignInView extends React.Component {
                 //do stuff
                 if (user) {
                     this.setState({currentUser:user.uid});
-                    
-                    // this.valueListener = ref.on("value", snapshot => this.setState({cardSnap: snapshot}));
-                    //this.state.userRef = ref;
+                    let ref = firebase.database().ref(`users`);
+                    this.valueListener = ref.on("value", snapshot => this.setState({userSnap: snapshot}));
+                    this.state.userRef = ref;
                 }
     
             }
@@ -32,29 +33,45 @@ export default class SignInView extends React.Component {
 
     componentWillUnmount() {
         this.unlistenAuth();
-        // this.state.tasksRef.off("value", this.valueListener);
     }
 
     handleSubmit(evt) {
         evt.preventDefault();
-        console.log(this.state.userName);
-        let userRef = firebase.database().ref(`users`);
-        let player = {
-            uid: this.state.currentUser,
-            displayName: this.state.userName,
-            points: 0
+        if (this.state.userName !== undefined) {
+            console.log(this.state.userName);
+            let player = {
+                uid: this.state.currentUser,
+                displayName: this.state.userName,
+                points: 0
+            }
+            console.log("A");
+            this.state.userRef.push(player);
+            this.state.players++;
+            console.log(this.state.userSnap.numChildren());
+            if (this.state.userSnap.numChildren() === 3) {
+                this.props.history.push(ROUTES.game);
+            }
         }
-        console.log("A");
-        userRef.push(player);
-        this.props.history.push(ROUTES.game);
     }
 
     render() {
+        let playersNames = [];
+        if (this.state.userSnap) {
+            this.state.userSnap.forEach(user => {
+                playersNames.push(<p>{user.val().displayName}</p>)
+            });
+        }
+        let waiting = null;
+        if (this.state.userSnap.numChildren() === 3) {
+            waiting = <div> waiting for more players to join...</div>;
+        }
+
         return(
             <div>
                 <header className="jumbotron jumbotron-fluid bg-primary text-white">
                     <div className="container-fluid">
                         <h1> Start a new game </h1>
+                        <h6> you must have 4 players to start the game </h6>
                     </div>
                 </header>
                 <form onSubmit={evt => this.handleSubmit(evt)}>
@@ -68,7 +85,14 @@ export default class SignInView extends React.Component {
                     </div>
                     <button className="btn btn-primary mb-2">Submit</button>
                 </form>
+                <div>
+                    <h2>Current Players</h2>
+                    {playersNames}
+                    {waiting}
+                </div>
             </div>
         );
     }
 }
+
+
