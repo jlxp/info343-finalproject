d590698c106df98d2888849d2d3dac6283f28082
TaraWilson17 <wwtara@hotmail.com>
2018-03-08 14:24:59 -0800

    Updating question answer logic

--- game/src/Card.js
+++ game/src/Card.js
@@ -28,60 +28,56 @@ export default class Card extends React.Component {
     // and moves played card to the current responses
     handleClick(evt, num) {
         evt.preventDefault();
-        let isUserCurrQuestionAsker = false;
-        let currUserUID;
+        this.setState({card: this.props.cardSnap.val()})
         this.props.usersSnap.forEach(userSnap => {
             let user = userSnap.val();
             if (user.index === this.props.userIndex) { // checks if current user is currently the question asker
-                isUserCurrQuestionAsker = user.questionAsker;
-                currUserUID = user.uid;
+                let currUserUID = user.uid;
+                if(!user.questionAsker) { // only allows users to play a card if they are not the current question askers
+                    if(this.state.answers < 1) {
+                        this.state.answers++;
+                        this.props.currResponsesRef.push({card: this.state.card}) // pass card data
+                            .catch(err => this.setState({fbError: err}));
+                    }
+                    firebase.database().ref(`gameState/currAnswerIndex`).once("value", snapshot => {
+                        let currNextIndex = snapshot.val(); // gets index of next answer index
+                        this.props.usersSnap.forEach(userSnap => {
+                            let i = 0; // keeps track of current card in a user's hand
+                            userSnap.child("cards").forEach(cardSnap => {
+                                i++;
+                                if(cardSnap.val() === num) {
+                                    this.props.whiteCardsRef.once("value", cardSnapshot => {
+                                        cardSnapshot.forEach(whiteCardSnap => {
+                                            let whiteCard = whiteCardSnap.val();
+                                            // if answer card index matches next answer card in deck index
+                                            if(whiteCard.index === currNextIndex) {
+                                                let nextIndexCardSnap = whiteCardSnap;
+                                                let key = whiteCardSnap.key;
+                                                // assigns newly dealt card to the current player index
+                                                firebase.database().ref(`cards/white_cards/${key}/playerIndex`).set(this.props.userIndex);
+                                                // creates an object with data for the new card to create card object
+                                                let cardObj = {
+                                                    userID: this.props.userID,
+                                                    whiteCardsRef: this.props.whiteCardsRef,
+                                                    cardSnap: nextIndexCardSnap,
+                                                    userIndex: this.props.userIndex
+                                                }
+                                                // replaces card in users current hand
+                                                this.props.replaceCardAtIndex(num, cardObj);
+                                                let cardStr = "card" + i;
+                                                firebase.database().ref(`users/${currUserUID}/cards/${cardStr}`).set(currNextIndex);
+                                                let nextIndex = currNextIndex + 1;
+                                                firebase.database().ref(`gameState/currAnswerIndex`).set(nextIndex);
+                                            } 
+                                        })
+                                    })
+                                }
+                            })
+                        })
+                    }) 
+                }
             }
         })
-        this.setState({card: this.props.cardSnap.val()})
-
-        if(!isUserCurrQuestionAsker) { // only allows users to play a card if they are not the current question askers
-            if(this.state.answers < 1) {
-                this.state.answers++;
-                this.props.currResponsesRef.push({card: this.state.card}) // pass card data
-                    .catch(err => this.setState({fbError: err}));
-            }
-            firebase.database().ref(`gameState/currAnswerIndex`).once("value", snapshot => {
-                let currNextIndex = snapshot.val(); // gets index of next answer index
-                this.props.usersSnap.forEach(userSnap => {
-                    let i = 0; // keeps track of current card in a user's hand
-                    userSnap.child("cards").forEach(cardSnap => {
-                        i++;
-                        if(cardSnap.val() === num) {
-                            this.props.whiteCardsRef.once("value", cardSnapshot => {
-                                cardSnapshot.forEach(whiteCardSnap => {
-                                    let whiteCard = whiteCardSnap.val();
-                                    // if answer card index matches next answer card in deck index
-                                    if(whiteCard.index === currNextIndex) {
-                                        let nextIndexCardSnap = whiteCardSnap;
-                                        let key = whiteCardSnap.key;
-                                        // assigns newly dealt card to the current player index
-                                        firebase.database().ref(`cards/white_cards/${key}/playerIndex`).set(this.props.userIndex);
-                                        // creates an object with data for the new card to create card object
-                                        let cardObj = {
-                                            userID: this.props.userID,
-                                            whiteCardsRef: this.props.whiteCardsRef,
-                                            cardSnap: nextIndexCardSnap,
-                                            userIndex: this.props.userIndex
-                                        }
-                                        // replaces card in users current hand
-                                        this.props.replaceCardAtIndex(num, cardObj);
-                                        let cardStr = "card" + i;
-                                        firebase.database().ref(`users/${currUserUID}/cards/${cardStr}`).set(currNextIndex);
-                                        let nextIndex = currNextIndex + 1;
-                                        firebase.database().ref(`gameState/currAnswerIndex`).set(nextIndex);
-                                    } 
-                                })
-                            })
-                        }
-                    })
-                })
-            }) 
-        }
     }
 
 
